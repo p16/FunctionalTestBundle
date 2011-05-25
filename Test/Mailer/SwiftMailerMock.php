3cdcb2a9604b36782228ca9e6bb0e5d137b624d2
Filippo De Santis <filippo.desantis@gmail.com>
2011-05-25 21:05:23 +0200

    Indentation

--- Test/Mailer/SwiftMailerMock.php
+++ Test/Mailer/SwiftMailerMock.php
@@ -6,37 +6,37 @@ use Liip\FunctionalTestBundle\Test\Mailer\SwiftMessageMock;
 
 class SwiftMailerMock
 {
-  protected $mail_path;
-  protected $message;
-  
-  public function __construct($mail_path = '/tmp/mail')
-  {
-    $this->mail_path = $mail_path;
-  }
 
-  public function createMessage()
-  {
-    return new SwiftMessageMock();
-  }
+    protected $mail_path;
+    protected $message;
 
-  public function send($message)
-  {
+    public function __construct($mail_path = '/tmp/mail')
+    {
+        $this->mail_path = $mail_path;
+    }
 
-    \file_put_contents($this->getAbsoluteMessagePath(join('.', \array_flip($message->getTo()))), \serialize($message));
-  }
+    public function createMessage()
+    {
+        return new SwiftMessageMock();
+    }
 
-  public function getMessage($to)
-  {
-    return \unserialize(\file_get_contents($this->getAbsoluteMessagePath($to)));
-  }
+    public function send($message)
+    {
+        \file_put_contents($this->getAbsoluteMessagePath(join('.', \array_flip($message->getTo()))), \serialize($message));
+    }
 
-  public function getAbsoluteMessagePath($to)
-  {
-    return $this->getAbsoluteMailPath().\DIRECTORY_SEPARATOR.$to.'.mail';
-  }
+    public function getMessage($to)
+    {
+        return \unserialize(\file_get_contents($this->getAbsoluteMessagePath($to)));
+    }
 
-  protected function getAbsoluteMailPath()
-  {
-    return \realpath($this->mail_path);
-  }
+    public function getAbsoluteMessagePath($to)
+    {
+        return $this->getAbsoluteMailPath() . \DIRECTORY_SEPARATOR . $to . '.mail';
+    }
+
+    protected function getAbsoluteMailPath()
+    {
+        return \realpath($this->mail_path);
+    }
 }
--- Tests/MailerMock/SwiftMailerMockTest.php
+++ Tests/MailerMock/SwiftMailerMockTest.php
@@ -6,27 +6,28 @@ use Liip\FunctionalTestBundle\Test\Mailer\SwiftMailerMock;
 
 class SwiftMailerMockTest extends \PHPUnit_Framework_TestCase
 {
-  protected $mailer;
+    protected $mailer;
 
-  public function setUp()
-  {
-    $this->mailer = new SwiftMailerMock();
-  }
+    public function setUp()
+    {
+        $this->mailer = new SwiftMailerMock();
+    }
 
-  public function test_getAbsoluteMessagePath()
-  {
-    $this->assertEquals(realpath('/tmp/mail/').'/cirpo@example.com.mail', $this->mailer->getAbsoluteMessagePath('cirpo@example.com'));
-  }
+    public function test_getAbsoluteMessagePath()
+    {
+        $this->assertEquals(realpath('/tmp/mail/') . '/cirpo@example.com.mail', $this->mailer->getAbsoluteMessagePath('cirpo@example.com'));
+    }
 
-  public function test_send()
-  {
-    $message = $this->getMock('Message', array('getTo'));
-    $message->expects($this->once())
-            ->method('getTo')
-            ->will($this->returnValue(array('bye@example.com' => 'Ciao')));
+    public function test_send()
+    {
+        $message = $this->getMock('Message', array('getTo'));
+        $message->expects($this->once())
+                ->method('getTo')
+                ->will($this->returnValue(array('bye@example.com' => 'Ciao')));
 
-    $this->mailer->send($message);
+        $this->mailer->send($message);
 
-    $this->assertEquals($message, $this->mailer->getMessage('bye@example.com'));
-  }
+        $this->assertEquals($message, $this->mailer->getMessage('bye@example.com'));
+    }
+
 }
